5d4b5b7ed92d00621b972960157999db2c286eb9
Nitish Varma <nitishvarma@Nitishs-MacBook-Pro.local>
2024-05-10 13:07:09 +0530

    Updated URL

--- devzery_middleware/middleware.py
+++ devzery_middleware/middleware.py
@@ -8,7 +8,7 @@ from django.utils.deprecation import MiddlewareMixin
 class RequestResponseLoggingMiddleware(MiddlewareMixin):
     def __init__(self, get_response=None):
         super().__init__(get_response)
-        self.api_endpoint = settings.DEVZERY_URL if settings.DEVZERY_URL else "http://localhost:8081/api"
+        self.api_endpoint = settings.DEVZERY_URL if settings.DEVZERY_URL else "https://server-v3-7qxc7hlaka-uc.a.run.app/api/add"
         self.api_key = settings.DEVZERY_API_KEY
         self.source_name = settings.DEVZERY_SOURCE_NAME
 
