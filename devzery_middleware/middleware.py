57cb9fcacc0862689dc01d0e08ab0fdc1c80edbe
devzery-clients <159508972+devzery-clients@users.noreply.github.com>
2024-06-29 12:47:55 +0530

    Changed Source Name to Server Name

--- devzery_middleware/middleware.py
+++ devzery_middleware/middleware.py
@@ -13,7 +13,7 @@ class RequestResponseLoggingMiddleware(MiddlewareMixin):
         self.api_endpoint = getattr(settings, 'DEVZERY_URL', "https://server-v3-7qxc7hlaka-uc.a.run.app/api/add")
         # self.api_endpoint = settings.DEVZERY_URL if settings.DEVZERY_URL else "https://server-v3-7qxc7hlaka-uc.a.run.app/api/add"
         self.api_key = settings.DEVZERY_API_KEY
-        self.source_name = settings.DEVZERY_SOURCE_NAME
+        self.source_name = settings.DEVZERY_SERVER_NAME
 
         print(self.api_endpoint)
 
